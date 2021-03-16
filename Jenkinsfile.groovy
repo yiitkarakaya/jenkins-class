1af6728decf700767666a5792fb920e2d5d6888b
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 04:43:02 +0000

    added properties

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,16 @@
+properties(
+	[parameters(
+		[choice(choices: 
+			[
+                'dev', 
+                'qa', 
+                'stage', 
+                'prod'
+            ], 
+		description: 'Which Environment should we deploy?', 
+		name: 'ENVIRONMENT')])]
+)
+
 node {
     stage("Stage1"){
         checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/infrastructure.git']]])
