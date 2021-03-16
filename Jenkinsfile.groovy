87234afb029c0583ede709f42d493bca01fa4808
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 04:32:05 +0000

    fixed spaces

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,17 +1,17 @@
 node {
-	stage("Stage1"){
-		git 'https://github.com/farrukh90/infrastructure.git'
-}
-	stage("Stage2"){
-       echo "hello"
-}
-	stage("Stage3"){
-		echo "hello"
-}
-	stage("Stage4"){
-		echo "hello"
-}
-	stage("Stage5"){
-		echo "hello"
+    stage("Stage1"){
+        git 'https://github.com/farrukh90/infrastructure.git'
+    }
+    stage("Stage2"){
+        echo "hello"
+    }
+    stage("Stage3"){
+        echo "hello"
+    }
+    stage("Stage4"){
+        echo "hello"
+    }
+    stage("Stage5"){
+        echo "hello"
 	}
 }
