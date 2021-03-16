66df49a8b50a5f9e2cd0060c3e445e0d98926cb6
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 02:45:40 +0000

    added parameter

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,6 +1,19 @@
+properties(
+	[parameters(
+		[choice(choices: 
+			[
+                'v0.1', 
+                'v0.2', 
+                'v0.3', 
+                'v0.4', 
+                'v0.5'
+            ], 
+		description: 'Which Environment should we deploy?', 
+		name: 'ENVIRONMENT')])]
+)
 node {
 	stage("Stage1"){
-		echo "hello"
+		git "https://github.com/farrukh90/jenkins-class.git"
 }
 	stage("Stage2"){
 		echo "hello"
