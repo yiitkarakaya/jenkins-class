02723df32d775f23d0ddbb1f55be6ede5b78b657
farrukh90 <farrukhsadykov@gmail.com>
2021-03-16 05:55:45 +0000

    added infrastructura

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -17,38 +17,38 @@ node {
         checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/infrastructure.git']]])
     }
     stage("Initialize"){
-        ws("workspace/inf/vpc"){
+        ws("workspace/infrastructura/vpc"){
             sh "export ENVIRONMENT=${ENVIRONMENT}"
             sh "make i"
         }
     }
     stage("Format"){
-        ws("workspace/inf/vpc"){
+        ws("workspace/infrastructura/vpc"){
             sh "export ENVIRONMENT=${ENVIRONMENT}"
             sh "make f"
         }
     }
     stage("Plan"){
-        ws("workspace/inf/vpc"){
+        ws("workspace/infrastructura/vpc"){
             sh "export ENVIRONMENT=${ENVIRONMENT}"
             sh "make p"
         }
     }
     stage("Apply"){
-         ws("workspace/inf/vpc"){
+         ws("workspace/infrastructura/vpc"){
             sh "export ENVIRONMENT=${ENVIRONMENT}"
             sh "make a"
         }
     }
     stage("destroy"){
-        ws("workspace/inf/vpc"){
+        ws("workspace/infrastructura/vpc"){
             sh "export ENVIRONMENT=${ENVIRONMENT}"
             sh "make d"
-            
+
         }
 	}
     stage("Clean"){
-        ws("workspace/inf/vpc"){
+        ws("workspace/infrastructura/vpc"){
             sh "export ENVIRONMENT=${ENVIRONMENT}"
             sh "make c"
         }
