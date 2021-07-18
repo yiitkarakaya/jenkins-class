5d504d77821d00d9e2659ae410d93ec8205d4b7e
farrukh90 <farrukhsadykov@gmail.com>
2021-07-18 13:24:30 +0000

    removed timer

--- Jenkinsfile_EC2Deployer.groovy
+++ Jenkinsfile_EC2Deployer.groovy
@@ -1,6 +1,6 @@
 node {
     properties(
-	[buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([cron('H/15 * * * *')]),
+	[buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
     parameters(
 		[string(defaultValue: 'apply', description: 'Should I build or destroy? ', name: 'TF_ACTION', trim: true),
         choice(choices: 
