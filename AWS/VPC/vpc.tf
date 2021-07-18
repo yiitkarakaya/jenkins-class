b39f4c22b49af5a37c05a564de76c67c963d8fe3
farrukh90 <farrukhsadykov@gmail.com>
2021-07-18 14:07:11 +0000

    enabled vpc dns

--- AWS/VPC/vpc.tf
+++ AWS/VPC/vpc.tf
@@ -1,6 +1,6 @@
 module "vpc" {
   source       = "farrukh90/vpc/aws"
-  version      = "7.0.0"
+  version      = "8.0.0"
   region       = var.vpc_config["region"]
   vpc_cidr     = var.vpc_config["vpc_cidr"]
   public_cidr1 = var.vpc_config["public_cidr1"]
