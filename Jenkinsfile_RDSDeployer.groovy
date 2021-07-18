d5e29937709a8a19e61dc13d75b9ff4a9b7fe91c
farrukh90 <farrukhsadykov@gmail.com>
2021-07-18 13:58:00 +0000

    added RDS Deployer

---
+++ Jenkinsfile_RDSDeployer.groovy
@@ -0,0 +1,23 @@
+node {
+    properties(
+	[buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), 
+    parameters(
+		[string(defaultValue: 'apply', description: 'Should I build or destroy? ', name: 'TF_ACTION', trim: true),
+        choice(choices: 
+			[
+                'dev', 
+            ], 
+		description: 'Which Environment should we deploy?', 
+		name: 'ENVIR')])])
+	stage("Clone a Repo"){
+        checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/jenkins-class.git']]])
+    }
+	stage("Build RDS"){
+		ws("${workspace}/AWS/RDS"){
+            sh "ENVIRONMENT=${ENVIR}   make tf-fmt  tf-init  tf-plan  tf-${TF_ACTION}"
+        }
+    }
+	stage("Send Notification"){
+		echo "hello"
+    }
+}
