8d14910a570e10b09b9067b3102bc7a6aba3c747
farrukh90 <farrukhsadykov@gmail.com>
2021-07-18 07:48:11 +0000

    added string

--- VPCDeployer.groovy
+++ VPCDeployer.groovy
@@ -1,7 +1,7 @@
 node {
     properties(
 	[buildDiscarder(logRotator(artifactDaysToKeepStr: '', artifactNumToKeepStr: '', daysToKeepStr: '', numToKeepStr: '5')), pipelineTriggers([cron('H/15 * * * *')]),
-    booleanParam(defaultValue: true, description: 'Apply/Destroy? Default Apply', name: 'TF_ACTION')],
+    string(defaultValue: 'apply', description: 'Should I build or destroy? ', name: 'TF_ACTION', trim: true),
     parameters(
 		[choice(choices: 
 			[
@@ -14,6 +14,7 @@ node {
 		name: 'ENVIR')])])
 
 
+        
 	stage("Clone a Repo"){
         checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/jenkins-class.git']]])
     }
