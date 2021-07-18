19657f261c6b199c67a2cb0518b029d452bcb60a
farrukh90 <farrukhsadykov@gmail.com>
2021-07-18 07:26:42 +0000

    added worskpace

--- VPCDeployer.groovy
+++ VPCDeployer.groovy
@@ -3,7 +3,7 @@ node {
         checkout([$class: 'GitSCM', branches: [[name: '*/main']], extensions: [], userRemoteConfigs: [[url: 'https://github.com/farrukh90/jenkins-class.git']]])
 }
 	stage("Stage2"){
-		ws("AWS/VPC"){
+		ws("${workspace}/AWS/VPC"){
             sh "pwd"
             // sh "ENVIRONMENT=dev   make tf-fmt  tf-init  tf-plan  tf-apply"
         }
